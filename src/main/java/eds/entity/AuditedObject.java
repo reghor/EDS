900a34b2d959cbb1ff3d4506ddbd139a3eedcf9c
hareluya86 <hareluya86@hotmail.com>
2015-03-15 01:06:51 +0800

    Audited object

---
+++ src/main/java/eds/entity/AuditedObject.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eds.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ *
+ * @author KH
+ */
+public abstract class AuditedObject implements Serializable {
+    
+    protected java.sql.Date DATE_CHANGED;
+    protected String CHANGED_BY;
+    protected java.sql.Date DATE_CREATED;
+    protected String CREATED_BY;
+
+    public Date getDATE_CREATED() {
+        return DATE_CREATED;
+    }
+
+    public void setDATE_CREATED(Date DATE_CREATED) {
+        this.DATE_CREATED = DATE_CREATED;
+    }
+
+    public Date getDATE_CHANGED() {
+        return DATE_CHANGED;
+    }
+
+    public void setDATE_CHANGED(Date DATE_CHANGED) {
+        this.DATE_CHANGED = DATE_CHANGED;
+    }
+
+    public String getCHANGED_BY() {
+        return CHANGED_BY;
+    }
+
+    public void setCHANGED_BY(String CHANGED_BY) {
+        this.CHANGED_BY = CHANGED_BY;
+    }
+
+    public String getCREATED_BY() {
+        return CREATED_BY;
+    }
+
+    public void setCREATED_BY(String CREATED_BY) {
+        this.CREATED_BY = CREATED_BY;
+    }
+    
+    
+}
