7b8d3d4a62eeb434c4354df359cbd964d6aaf1b9
hareluya86 <hareluya86@hotmail.com>
2015-08-15 22:30:25 +0800

    RelationshipExistsException

--- src/main/java/eds/component/data/RelationshipExistsException.java
+++ src/main/java/eds/component/data/RelationshipExistsException.java
@@ -5,14 +5,24 @@
  */
 package eds.component.data;
 
+import eds.entity.data.EnterpriseObject;
+import eds.entity.data.EnterpriseRelationship;
+
 /**
  *
  * @author LeeKiatHaw
+ * @param <R>
  */
-public class RelationshipExistsException extends Exception{
-
-    public RelationshipExistsException(String message) {
+public class RelationshipExistsException extends Exception {
+    /*public RelationshipExistsException(String message) {
         super(message);
+    }*/
+    
+    public <R extends EnterpriseRelationship> RelationshipExistsException(R r){
+        super("Relationship "+r.getClass().getSimpleName() 
+        + ((r.getSOURCE() != null) ? "" : (" with SOURCE "+r.getSOURCE_TYPE()+" (ID:"+r.getSOURCE().getOBJECTID()+", NAME:"+r.getSOURCE().getOBJECT_NAME()+")"))
+        + ((r.getTARGET() != null) ? "" : (" with TARGET "+r.getTARGET_TYPE()+" (ID:"+r.getTARGET().getOBJECTID()+", NAME:"+r.getTARGET().getOBJECT_NAME()+")"))
+        +" already exists.");
     }
     
 }
